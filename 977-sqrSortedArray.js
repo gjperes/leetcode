623bbf22336e71ce04014dc1fb534acbd10a6185
Gabriel <61798393+gjperes@users.noreply.github.com>
2022-02-20 17:20:29 -0300

    best practices fix

--- 977-sqrSortedArray.js
+++ 977-sqrSortedArray.js
@@ -2,7 +2,7 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortedSquares = function(nums) {
+const sortedSquares = function(nums) {
     let sqrnums = []
     for(let i=0; i<nums.length; i++){
         let sqr = nums[i] * nums[i]
